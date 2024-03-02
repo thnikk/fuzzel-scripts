cd8282d3b73bfc60d961c91589991a070230b97a
thnikk <thnikkshop@gmail.com>
2024-03-02 11:08:01 -0800

    Added blacklist/whitelist arguments

--- fuzzel-vm.py
+++ fuzzel-vm.py
@@ -8,6 +8,15 @@ from subprocess import Popen, PIPE, run
 import sys
 import json
 import os
+import argparse
+
+parser = argparse.ArgumentParser(description="VM fuzzel launcher")
+parser.add_argument(
+    'filter', type=str,
+    nargs='*', help="Filter for blacklist")
+parser.add_argument('-w', action='store_true',
+                    help="Change blacklist to whitelist.")
+args = parser.parse_args()
 
 
 def vm_list() -> list:
@@ -57,6 +66,19 @@ def sort_dict(dictionary) -> dict:
     return dict(sorted(dictionary.items(), key=lambda x: x[1], reverse=True))
 
 
+def filter_list(item_list, filter_strings, invert=False) -> list:
+    """ Filter items out of list """
+    temp_list = []
+    for item in item_list:
+        for filter_string in filter_strings:
+            print(filter_string, item, filter_string in item)
+            if filter_string in item:
+                temp_list.append(item)
+    if invert:
+        return temp_list
+    return list(set(item_list) - set(temp_list))
+
+
 def main():
     """ Main function """
     cache_file = os.path.expanduser('~/.cache/fuzzel-vm.json')
@@ -70,7 +92,8 @@ def main():
     sorted_cache = sort_some(vm_list(), list(sort_dict(counted)))
     active = vm_active()
     sorted_active = sort_some(sorted_cache, active)
-    selection = get_selection(sorted_active)
+    filtered_active = filter_list(sorted_active, args.filter, args.w)
+    selection = get_selection(filtered_active)
     if selection in active:
         operation = get_selection(["shutdown", "reboot", "destroy"],
                                   "Select an option: ")
